856b02fcdca0c9301067f05132ee57b4a936905e
Mario Augusto <mario.aparecido@openet.com>
2020-12-22 21:46:02 -0300

    final fix to product tests

--- src/test/java/teste/ProductControllerTest.java
+++ src/test/java/teste/ProductControllerTest.java
@@ -1,6 +1,6 @@
 package teste;
 
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,44 +21,46 @@ import com.pisi.marketplace.data.entity.Product;
 import com.pisi.marketplace.resource.model.ProductResource;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=ProductController.class)
-public class ProductControllerTest  {
+@SpringBootTest(classes = ProductController.class)
+public class ProductControllerTest {
 
 	@MockBean
 	private ProductService productService;
-	
+
 	@Autowired
 	private ProductController controller;
-	
-	
+
 	@Test
-	public void findProducts() throws Exception{
-		List<Product> productList = controller.searchProducts();
-		Mockito.when(productService.findAllProducts()).thenReturn(productList);
-		Assert.assertNotNull(productList);
-		}
-	
-	@Test
-	public void registerProduct() throws Exception{
-		ProductResource product = new ProductResource();
-		controller.registerProduct(product);
-	    
-	 
+	public void findProducts() throws Exception {
+		List<Product> productListToReturn = new ArrayList<Product>();
+		Mockito.when(productService.findAllProducts()).thenReturn(productListToReturn);
+		List<Product> productListReturned = controller.searchProducts();
+		Assert.assertNotNull(productListReturned);
 	}
 
 	@Test
-	public void findProductsBySearch() throws Exception{
-		String toFind = "cartola";
-		List<Product> product = controller.findProductsBySearch(toFind);
-		Mockito.when(productService.findAllProductsByName(toFind)).thenReturn(product);
-		System.out.println(product);
-		//Assert.assertEquals(produto.contains(txtBuscar), true);
+	public void registerProduct() throws Exception {
+		ProductResource product = new ProductResource();
+		Mockito.when(productService.registerProduct(product)).thenReturn(1);
+		int result = controller.registerProduct(product);
+		Assert.assertEquals(1, result);
 	}
-	
+
 	@Test
-	public void findProductsById() throws Exception{
+	public void findProductsBySearch() throws Exception {
+		List<Product> productsToReturn = new ArrayList<Product>();
+		String toFind = "shoes";
+		Mockito.when(productService.findAllProductsByName(toFind)).thenReturn(productsToReturn);
+		List<Product> productsReturned = controller.findProductsBySearch(toFind);
+		Assert.assertEquals(productsToReturn, productsReturned);
+	}
+
+	@Test
+	public void findProductsById() throws Exception {
 		long idToFind = 1;
-		Optional<Product> product = controller.findProductsById(idToFind);
-		Assert.assertFalse(product.isPresent());//errad
+		Optional<Product> productToReturn = Optional.empty();
+		Mockito.when(productService.findProductsById(idToFind)).thenReturn(productToReturn);
+		Optional<Product> productReturned = controller.findProductsById(idToFind);
+		Assert.assertEquals(productToReturn, productReturned);
 	}
 }
